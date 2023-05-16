ebb590502c68acad200d67ad5c7a42ba84634ce9
Maze77-AH <nicholaslasagna@gmail.com>
2023-05-15 17:06:58 -0700

    first commit

---
+++ DonkeyKong3.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.*;
+import java.io.IOException;
+
+public class DonkeyKong3 extends JFrame implements KeyListener, ActionListener {
+    
+    public void keyPressed(KeyEvent event) {
+        if () {
+
+        }
+    }
+}
---
+++ DonkeyKong3Main.java
@@ -0,0 +1,5 @@
+public class DonkeyKong3Main {
+    public static void main(String[] args) {
+        new DonkeyKong3();
+    }
+}
