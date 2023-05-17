2d31a92e5495a9f6bf5b554160f621a3defe906e
Maze77-AH <nicholaslasagna@gmail.com>
2023-05-16 19:02:32 -0700

    Move

--- DonkeyKong3.java
+++ DonkeyKong3.java
@@ -15,6 +15,7 @@ public class DonkeyKong3 extends JFrame implements KeyListener, ActionListener {
     private int windowWidth = 1000;
     private int windowHeight = 865;
     private boolean up, down, left, right;
+    private int fps = 60;
 
     int playerX = 0;
     int playerY = 75;
@@ -38,8 +39,8 @@ public class DonkeyKong3 extends JFrame implements KeyListener, ActionListener {
         myFrame.setLocationRelativeTo(null);
         myFrame.setVisible(true);
         this.lives = lives;
-        loop();
         myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setFPSandPaint();
         try {
             setHighScore();
         } catch (IOException e) {
@@ -47,10 +48,35 @@ public class DonkeyKong3 extends JFrame implements KeyListener, ActionListener {
         }
     }
 
-    public void loop() {
-        while(myFrame != null)
-            update();
-            repaint();
+    public void setFPSandPaint() {
+
+        // Set FPS (information found on Stack Overflow)
+
+        double drawInterval = 1000000000/fps;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime = System.nanoTime();
+        int timer = 0;
+        int drawCount = 0;
+
+        while(myFrame != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+            if (timer >= 1000000000) {
+                System.out.println("FPS " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
     }
 
     public void setHighScore() throws IOException {
@@ -82,16 +108,36 @@ public class DonkeyKong3 extends JFrame implements KeyListener, ActionListener {
         if (level == 0) {
             myFrame.add(new JLabel(new ImageIcon("dk3_level_0.png")));
         }
+        else if (level == 1) {
+            myFrame.add(new JLabel(new ImageIcon("dk3_level_1.png")));
+        }
+        else if (level == 2) {
+            myFrame.add(new JLabel(new ImageIcon("dk3_level_2.png")));
+        }
+        else {
+            new DonkeyKong3(currentScore, 0, lives);
+            myFrame.dispatchEvent(new WindowEvent(myFrame, WindowEvent.WINDOW_CLOSING));
+        }
+            
     }
 
     public void update() {
-
+        if(up)
+            playerY -= playerSpeed;
+        else if(left)
+            playerX -= playerSpeed;
+        else if(right)
+            playerX += playerSpeed;
+        else if(down)
+            playerY += playerSpeed;
     }
 
     public void paintComponent(Graphics g) {
+        super.paintComponents(g);
+        System.out.println("MOVING");
         Graphics2D g2 = (Graphics2D)g;
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, 100, 100);
+        g2.setColor(Color.red);
+        g2.fillRect(playerX, playerY, 800, 800);
         g2.dispose();
     }
 
